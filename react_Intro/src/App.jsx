03c401ea05c5ee60a57edeed89bfd6237e7f3e42
Suryakant Dwivedi <suryakantdwivedi8493@gmail.coms>
2026-01-20 03:38:45 +0530

    react-Intro done

---
+++ react_Intro/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div id="dad">
+        <h1> Hello World </h1>
+        <p> This is my first React App </p>
+      </div>
+      <div id="mom">
+        <h2> Welcome to ReactJS </h2>
+        <p> Let's build something amazing together! </p>
+      </div>
+    </>
+  )
+}
+
+export default App
